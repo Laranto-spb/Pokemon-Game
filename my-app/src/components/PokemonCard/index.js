a579eced3db3f080c96f567ae46a59677763d507
Laranto-spb <laranto1985@gmail.com>
2021-03-19 16:37:15 +0300

    feat: Task#1: Cards from firebase without opening

--- my-app/src/components/PokemonCard/index.js
+++ my-app/src/components/PokemonCard/index.js
@@ -2,7 +2,7 @@ import s from './style.module.css';
 import cardBackSide from './assets/card-back-side.jpg';
 import cn from 'classnames';
 
-const PokemonCard = ({name, img, id, type, values, onClickCard, isActive}) => {
+const PokemonCard = ({pokemonKey, name, img, id, type, values, onClickCard, isActive}) => {
     const handleClicker = () => {
         onClickCard(id);
     }
