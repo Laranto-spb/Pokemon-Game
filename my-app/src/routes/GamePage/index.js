d05f96ae2ba13e65aeccd509d933f663dd73f791
Laranto-spb <laranto1985@gmail.com>
2021-03-26 10:49:26 +0300

    feat: Final page withot choice

--- my-app/src/routes/GamePage/index.js
+++ my-app/src/routes/GamePage/index.js
@@ -8,7 +8,8 @@ import {PokemonContext} from "../../context/pokemonContext";
 const GamePage = () => {
     const match = useRouteMatch();
     const [selectedPokemon, setSelectedPokemon] = useState({});
-
+    const [finalPokemons1, setFinalPokemons1] = useState({})
+    const [finalPokemons2, setFinalPokemons2] = useState({})
 
     const handlerSelectedPokemon = (key, pokemon) => {
         setSelectedPokemon(prevState => {
@@ -25,11 +26,19 @@ const GamePage = () => {
         });
     }
 
+    const getFinalPokemons = (finalPlayer1, finalPlayer2) => {
+        finalPlayer1 && setFinalPokemons1(finalPlayer1);
+        finalPlayer2 && setFinalPokemons2(finalPlayer2);
+    }
 
     return (
         <PokemonContext.Provider value={{
             pokemons: Object.values(selectedPokemon),
-            onSelectedPokemon: handlerSelectedPokemon
+            onSelectedPokemon: handlerSelectedPokemon,
+            onFinalPokemons: getFinalPokemons,
+            finalPokemons1: finalPokemons1,
+            finalPokemons2: finalPokemons2,
+
         }}>
             <Switch>
                 <Route path={`${match.path}/`} exact component={StartPage}/>
--- my-app/src/routes/GamePage/routes/Board/index.js
+++ my-app/src/routes/GamePage/routes/Board/index.js
@@ -6,10 +6,29 @@ import {useHistory} from 'react-router-dom';
 import {useEffect, useState} from 'react';
 import PlayerBoard from "./component/PlayerBoard";
 
+
+const counterWin = (board, player1, player2) => {
+    let player1Count = player1.length;
+    let player2Count = player2.length;
+
+    board.forEach(item => {
+            if (item.card.possession === 'red') {
+                player1Count++
+            } else if (item.card.possession === 'blue') {
+                player2Count++
+            }
+        }
+    )
+
+    return [player1Count, player2Count];
+
+}
+
 const BoardPage = () => {
 
     const pokemonsContext = useContext(PokemonContext);
     const pokemons = pokemonsContext.pokemons;
+
 
     const history = useHistory({});
     const [board, setBoard] = useState([]);
@@ -20,10 +39,12 @@ const BoardPage = () => {
         }))
     })
     const [player2, setPlayer2] = useState([]);
+    const [initialPlayer2, setInitialPlayer2] = useState([]);
     const [choiceCard, setChoiceCard] = useState(null)
+    const [steps, setSteps] = useState(0);
+
 
     const handlerClickBoardPlate = async (position) => {
-        console.log(position);
         if (choiceCard) {
             const params = {
                 position,
@@ -39,11 +60,38 @@ const BoardPage = () => {
             });
 
             const request = await res.json();
+
+
+            if (choiceCard.player === 1) {
+                setPlayer1(prevState => prevState.filter(item => item.id !== choiceCard.id))
+            }
+            if (choiceCard.player === 2) {
+                setPlayer2(prevState => prevState.filter(item => item.id !== choiceCard.id))
+            }
+
             setBoard(request.data);
+            setSteps(prevState => {
+                    const count = prevState + 1;
+                    return count;
+                }
+            )
         }
     }
 
-    console.log(choiceCard);
+    useEffect(() => {
+        if (steps === 9) {
+            const [count1, count2] = counterWin(board, player1, player2);
+            if (count1 > count2) {
+                alert('WIN');
+            } else if (count1 < count2) {
+                alert('LOSE');
+            } else {
+                alert('DRAW');
+            }
+
+            history.replace('/game/finish');
+        }
+    }, [steps])
 
     useEffect(async () => {
         const boardResponce = await fetch('https://reactmarathon-api.netlify.app/api/board');
@@ -52,6 +100,8 @@ const BoardPage = () => {
 
         const player2Response = await fetch('https://reactmarathon-api.netlify.app/api/create-player');
         const player2Request = await player2Response.json();
+        setInitialPlayer2(player2Request.data);
+
         setPlayer2(() => {
             return player2Request.data.map(item => ({
                 ...item,
@@ -59,12 +109,17 @@ const BoardPage = () => {
             }))
         });
 
+        pokemonsContext.onFinalPokemons(pokemons,initialPlayer2);
+
 
     }, [])
 
-    // if(Object.keys(pokemons).length === 0) {
-    //     history.replace('/game')
-    // }
+
+    if(Object.keys(pokemons).length === 0) {
+        history.replace('/game')
+    }
+
+
     return (
         <div className={s.root}>
             <div className={s.playerOne}>
